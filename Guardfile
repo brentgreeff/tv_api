7dcdbdd3c2d80b300cd1a7f70518e507f9a5fbdf
Brent <brentgreeff@gmail.com>
2020-03-04 21:50:52 +0200

    Fixing Rubocop warnings.

--- Guardfile
+++ Guardfile
@@ -1,4 +1,4 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 
 # https://github.com/guard/guard#readme
 
--- app/serializable/serializable_movie.rb
+++ app/serializable/serializable_movie.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 
 class SerializableMovie < JSONAPI::Serializable::Resource
   type 'movies'
